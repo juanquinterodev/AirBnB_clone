d762da246fa2f447cfeee2709239479c5da40eaf
02KellyV <900@holbertonschool.com>
2019-11-12 18:50:04 +0000

    pycodestyle console

--- console.py
+++ console.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-import cmd, sys
+import cmd
+import sys
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hnbn) '
     file = None
-    def do_quit(self, inp):
-        pass
 
+    def do_quit(self, inp):
         return True
+
     def help_quit(self):
         print('Quit command to exit the program')
+
     def emptyline(self):
         pass
+
     do_EOF = do_quit
     help_EOF = help_quit
+
+
 """quit
 EOF
 do_help()
